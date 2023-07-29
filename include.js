88a392d1c6c320da74634e06a9ea5b3a33a03f75
Muhammad Shakir <chaudhryshakir67@gmail.com>
2023-07-29 16:43:49 +0500

    Include method in Js

---
+++ include.js
@@ -0,0 +1,5 @@
+// includes : check that the elemnt you are looking for is present in the array or not and gives you answer in true and false.
+
+console.log(team1.includes("Shakir")); // true
+console.log(team2.includes("Haris")); // true
+console.log(team3.includes("Ali")); // false
